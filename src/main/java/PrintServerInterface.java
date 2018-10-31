16ad06247358dfbbec2104d98498c4b9dd45c1b8
Arvid Langsø <arvid.k.langsoe@live.dk>
2018-10-31 16:11:34 +0100

    Added Job and Interface

---
+++ src/main/java/PrintServerInterface.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public interface PrintServerInterface {
+
+    public void print(String token, String filename, String printer);
+    public List<Job> queue(String token);
+    public void topQueue(String token, int job);
+    public void start(String token);
+    public void stop(String token);
+    public void restart(String token);
+    public String status(String token);
+    public void readConfig(String token, String parameter);
+    public void setConfig(String token, String parameter, String value);
+    public void login(String username, String password);
+    public void logout(String token);
+
+}
