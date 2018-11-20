588198445bc165b6db0f98ace1245a97aeae6881
peterxp@hotmail.fr <9875321elhabr>
2018-11-20 16:40:21 +0100

    PrinterPermissionManager implementation

--- src/main/java/permissions/PrinterPermissionManager.java
+++ src/main/java/permissions/PrinterPermissionManager.java
@@ -1,4 +1,22 @@
 package permissions;
 
-public class PrinterPermissionManager {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PrinterPermissionManager implements PermissionManager {
+
+    HashMap<String, ArrayList<Permissions>> permissions = new HashMap<>();
+
+    @Override
+    public List<Permissions> getPermissionsOfUser(String userName) {
+        return null;
+    }
+
+    @Override
+    public boolean userHasPermission(String userName, Permissions permission) {
+        return false;
+    }
+
+
 }
